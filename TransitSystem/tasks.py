9fce333a6771eb26b04af6a1efdd895248a7de03
zarroc <gmail.com>
2014-06-15 00:38:33 -0400

    initial

---
+++ TransitSystem/tasks.py
@@ -0,0 +1,69 @@
+from __future__ import absolute_import
+import urllib
+from celery import task
+from TransitSystem import gtfs_realtime_pb2
+from TransitSystem.models import Trips, Routes, VehiclePositions, TripUpdates,\
+    StopTimes, Stops
+import django.core.exceptions
+from django.core.exceptions import ObjectDoesNotExist
+from pytz import timezone
+from datetime import datetime, tzinfo
+import pytz
+__author__ = 'zarroc'
+
+
+@task(name='loadrealtimedata')
+def load_realtime():
+    files=urllib.urlretrieve("http://rtu.york.ca/gtfsrealtime/VehiclePositions","/home/zarroc/workspace/bustransit/Bustransit/TransitSystem/VehiclePositions")
+    print files[0]
+    print files[1]
+    print "File Downloaded"
+    f = open("/home/zarroc/workspace/bustransit/Bustransit/TransitSystem/VehiclePositions", "rb")
+    data=gtfs_realtime_pb2.FeedMessage()
+    data.ParseFromString(f.read())
+    for entity in data.entity:
+        ventity=entity.vehicle
+        try:
+            trip=Trips.objects.get(trip_id=ventity.trip.trip_id)
+            if ventity.trip.route_id !="":
+                route=Routes.objects.get(route_id=ventity.trip.route_id)
+            if trip.trip_id==599516:
+                print ventity.position.latitude
+                print ventity.position.longitude
+            vp=VehiclePositions.objects.filter(trip_id=trip.trip_id)
+            if not vp.exists():
+                vp=VehiclePositions(route_id=None,trip_id=trip,p_latitude=ventity.position.latitude,p_longitude=ventity.position.longitude,p_bearing=ventity.position.bearing,p_speed=ventity.position.speed)
+            else:
+                vp=vp[0]
+                vp.p_latitude=ventity.position.latitude
+                vp.p_longitude=ventity.position.longitude
+                vp.p_bearing=ventity.position.bearing
+                vp.p_speed=ventity.position.speed
+            vp.save()
+        except ObjectDoesNotExist:
+            print "trip id not found"
+            
+@task(name='loadTripUpdates')
+def load_TripUpdates():
+    urllib.urlretrieve("http://rtu.york.ca/gtfsrealtime/TripUpdates","/home/zarroc/workspace/bustransit/Bustransit/TransitSystem/TripUpdates")
+    print "File Downloaded"
+    f = open("/home/zarroc/workspace/bustransit/Bustransit/TransitSystem/TripUpdates", "rb")
+    data=gtfs_realtime_pb2.FeedMessage()
+    data.ParseFromString(f.read())
+    eastern = timezone('US/Eastern')
+    for entity in data.entity:
+        ventity=entity.trip_update
+        for  stopTimeUpdate in ventity.stop_time_update:
+            tu=TripUpdates.objects.filter(trip_id=ventity.trip.trip_id,stop_sequence=int(stopTimeUpdate.stop_sequence))
+            if tu:
+                for t in tu:
+                    if t.stop_sequence==int(stopTimeUpdate.stop_sequence):
+                        t.arrival_time=datetime.fromtimestamp(int(stopTimeUpdate.arrival.time),tz=eastern).strftime('%H:%M:%S')
+                        t.departure_time=datetime.fromtimestamp(int(stopTimeUpdate.departure.time),tz=eastern).strftime('%H:%M:%S')
+                        t.save()
+            else:
+                stop=StopTimes.objects.filter(trip_id=ventity.trip.trip_id,stop_sequence=int(stopTimeUpdate.stop_sequence))
+                if stop:
+                    stop=stop[0]
+                    vp=TripUpdates(trip_id=stop.trip_id,stop_id=stop.stop_id,stop_sequence=int(stopTimeUpdate.stop_sequence),arrival_time=datetime.fromtimestamp(int(stopTimeUpdate.arrival.time)).strftime('%H:%M:%S'),departure_time=datetime.fromtimestamp(int(stopTimeUpdate.departure.time)).strftime('%H:%M:%S'),timestamp=datetime.fromtimestamp(int(ventity.timestamp)).strftime('%H:%M:%S'))                                                                   
+                    vp.save()
---
+++ TransitSystem/views.py
@@ -0,0 +1,109 @@
+# Create your views here.
+from datetime import datetime
+
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from TransitSystem.forms import SearchForm
+from models import VehiclePositions, Trips, Routes, StopTimes
+from TransitSystem.models import TripUpdates, Stops, CalendarDates
+from TransitSystem import gtfs_realtime_pb2
+import urllib
+from operator import itemgetter
+
+
+@login_required
+def home(request):
+    return render_to_response('index.html')
+
+@login_required
+def findbus(request):
+    form=SearchForm(request.POST)
+    return render_to_response('findbus.html',{'form':form},context_instance=RequestContext(request))
+
+def searchbus(request):
+    if "routeNo" in request.GET:
+        tripdetails=[]
+        date=request.GET['searchdate']
+        time=request.GET['searchtime']
+        date=datetime.strptime(date,'%Y/%m/%d')
+        service_id=findserviceId(date)
+        print service_id
+        print type(time)
+        routes=Routes.objects.filter(route_short_name=request.GET['routeNo'])
+        for route in routes:
+            trips=Trips.objects.filter(route_id=route.route_id,service_id__in=service_id)
+            for trip in trips:
+                result={}
+                stops=StopTimes.objects.filter(trip_id=trip.trip_id,stop_sequence=1,arrival_time__gt=time)
+                for stop in stops:
+                    result['tripName']=trip.trip_headsign
+                    result['trip_id']=trip.trip_id
+                    result['startTime']=stop.arrival_time
+                if result:
+                    tripdetails.append(result)
+    return render_to_response('results.html',{'stop_times':tripdetails})
+
+
+def schedule(request):
+    if "trip_id" in request.GET:
+        stoptimes=[]
+        trip_id=request.GET['trip_id']
+        schedules=StopTimes.objects.filter(trip_id=trip_id)
+        for schedule in schedules:
+            stoptime={}
+            print schedule.stop_id
+            stoptime["stop_name"]=schedule.stop_id.stop_name
+            stoptime["arrival_time"]=schedule.arrival_time
+            stoptime["departure_time"]=schedule.departure_time
+            stoptimes.append(stoptime)
+        print stoptimes
+    return render_to_response('schedule.html',{'stoptimes':stoptimes})
+
+def locate(request):
+    trip_id=request.GET['trip_id']
+    stoplocs=StopTimes.objects.filter(trip_id=trip_id)
+    if stoplocs:
+        stop_locations=[]
+        for stoploc in stoplocs:
+            stops={}
+            stops['lat']=stoploc.stop_id.stop_lat
+            stops['lon']=stoploc.stop_id.stop_lon
+            stop_locations.append(stops)
+    vp=VehiclePositions.objects.filter(trip_id=trip_id)
+    vehiclepos={}
+    if vp:
+        vehicleposition=vp[0];
+        vehiclepos['lat']=vehicleposition.p_latitude
+        vehiclepos['lon']=vehicleposition.p_longitude
+    return render_to_response('locate.html',{'stoplocations':stop_locations,'vp':vehiclepos})
+
+
+def predict(request):
+    if "trip_id" in request.GET:
+        stoptimes=[]
+        trip_id=request.GET['trip_id']
+        schedules=TripUpdates.objects.filter(trip_id=trip_id)
+        for schedule in schedules:
+            stoptime={}
+            print schedule.stop_id
+            stoptime["stop_name"]=schedule.stop_id.stop_name
+            stoptime["arrival_time"]=schedule.arrival_time
+            stoptime["departure_time"]=schedule.departure_time
+            stoptimes.append(stoptime)
+    return render_to_response('predictions.html',{'stoptimes':stoptimes})
+    
+def findserviceId(date):
+    date=date.date()
+    result=[]
+    dayofweek=date.weekday()
+    if (dayofweek>=0 and dayofweek<5):
+        result.append(1)
+    else:
+        result.append(80)
+    service_ids=CalendarDates.objects.filter(date=datetime.strftime(date,'%Y%m%d'),exception_type=1)
+    for service_id in service_ids:
+        result.append(service_id.service_id.service_id)
+    return result    
