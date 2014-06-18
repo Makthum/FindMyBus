08f315e9f7aba2abcf49bd9b6bd6eab553de93ef
Mohamed Makthum Mohamed <makthum@gmail.com>
2014-06-17 22:52:35 -0400

    Update views.py

--- TransitSystem/views.py
+++ TransitSystem/views.py
@@ -24,77 +24,90 @@ def findbus(request):
     return render_to_response('findbus.html',{'form':form},context_instance=RequestContext(request))
 
 def searchbus(request):
-    if "routeNo" in request.GET:
-        tripdetails=[]
-        date=request.GET['searchdate']
-        time=request.GET['searchtime']
-        date=datetime.strptime(date,'%Y/%m/%d')
-        service_id=findserviceId(date)
-        print service_id
-        print type(time)
-        routes=Routes.objects.filter(route_short_name=request.GET['routeNo'])
-        for route in routes:
-            trips=Trips.objects.filter(route_id=route.route_id,service_id__in=service_id)
-            for trip in trips:
-                result={}
-                stops=StopTimes.objects.filter(trip_id=trip.trip_id,stop_sequence=1,arrival_time__gt=time)
-                for stop in stops:
-                    result['tripName']=trip.trip_headsign
-                    result['trip_id']=trip.trip_id
-                    result['startTime']=stop.arrival_time
-                if result:
-                    tripdetails.append(result)
-    return render_to_response('results.html',{'stop_times':tripdetails})
-
+    try:
+        if "routeNo" in request.GET:
+            tripdetails=[]
+            date=request.GET['searchdate']
+            time=request.GET['searchtime']
+            date=datetime.strptime(date,'%Y/%m/%d')
+            service_id=findserviceId(date)
+            print service_id
+            print type(time)
+            routes=Routes.objects.filter(route_short_name=request.GET['routeNo'])
+            for route in routes:
+                trips=Trips.objects.filter(route_id=route.route_id,service_id__in=service_id)
+                for trip in trips:
+                    result={}
+                    stops=StopTimes.objects.filter(trip_id=trip.trip_id,stop_sequence=1,arrival_time__gt=time)
+                    for stop in stops:
+                        result['tripName']=trip.trip_headsign
+                        result['trip_id']=trip.trip_id
+                        result['startTime']=stop.arrival_time
+                    if result:
+                        tripdetails.append(result)
+        return render_to_response('results.html',{'stop_times':tripdetails})
+    except:
+        e = sys.exc_info()[0]
+        return render_to_response('results.html',{'error':e})
 
 def schedule(request):
-    if "trip_id" in request.GET:
-        stoptimes=[]
-        trip_id=request.GET['trip_id']
-        schedules=StopTimes.objects.filter(trip_id=trip_id)
-        for schedule in schedules:
-            stoptime={}
-            print schedule.stop_id
-            stoptime["stop_name"]=schedule.stop_id.stop_name
-            stoptime["arrival_time"]=schedule.arrival_time
-            stoptime["departure_time"]=schedule.departure_time
-            stoptimes.append(stoptime)
-        print stoptimes
-    return render_to_response('schedule.html',{'stoptimes':stoptimes})
+    try:
+        if "trip_id" in request.GET:
+            stoptimes=[]
+            trip_id=request.GET['trip_id']
+            schedules=StopTimes.objects.filter(trip_id=trip_id)
+            for schedule in schedules:
+                stoptime={}
+                print schedule.stop_id
+                stoptime["stop_name"]=schedule.stop_id.stop_name
+                stoptime["arrival_time"]=schedule.arrival_time
+                stoptime["departure_time"]=schedule.departure_time
+                stoptimes.append(stoptime)
+            print stoptimes
+        return render_to_response('schedule.html',{'stoptimes':stoptimes})
+    except:
+    e = sys.exc_info()[0]
+    return render_to_response('results.html',{'error':e})
 
 def locate(request):
-    trip_id=request.GET['trip_id']
-    stoplocs=StopTimes.objects.filter(trip_id=trip_id)
-    if stoplocs:
-        stop_locations=[]
-        for stoploc in stoplocs:
-            stops={}
-            stops['lat']=stoploc.stop_id.stop_lat
-            stops['lon']=stoploc.stop_id.stop_lon
-            stop_locations.append(stops)
-    vp=VehiclePositions.objects.filter(trip_id=trip_id)
-    vehiclepos={}
-    if vp:
-        vehicleposition=vp[0];
-        vehiclepos['lat']=vehicleposition.p_latitude
-        vehiclepos['lon']=vehicleposition.p_longitude
-    return render_to_response('locate.html',{'stoplocations':stop_locations,'vp':vehiclepos})
-
+    try:
+        trip_id=request.GET['trip_id']
+        stoplocs=StopTimes.objects.filter(trip_id=trip_id)
+        if stoplocs:
+            stop_locations=[]
+            for stoploc in stoplocs:
+                stops={}
+                stops['lat']=stoploc.stop_id.stop_lat
+                stops['lon']=stoploc.stop_id.stop_lon
+                stop_locations.append(stops)
+        vp=VehiclePositions.objects.filter(trip_id=trip_id)
+        vehiclepos={}
+        if vp:
+            vehicleposition=vp[0];
+            vehiclepos['lat']=vehicleposition.p_latitude
+            vehiclepos['lon']=vehicleposition.p_longitude
+        return render_to_response('locate.html',{'stoplocations':stop_locations,'vp':vehiclepos})
+    except:
+        e = sys.exc_info()[0]
+        return render_to_response('results.html',{'error':e})
 
 def predict(request):
-    if "trip_id" in request.GET:
-        stoptimes=[]
-        trip_id=request.GET['trip_id']
-        schedules=TripUpdates.objects.filter(trip_id=trip_id)
-        for schedule in schedules:
-            stoptime={}
-            print schedule.stop_id
-            stoptime["stop_name"]=schedule.stop_id.stop_name
-            stoptime["arrival_time"]=schedule.arrival_time
-            stoptime["departure_time"]=schedule.departure_time
-            stoptimes.append(stoptime)
-    return render_to_response('predictions.html',{'stoptimes':stoptimes})
-    
+    try:
+        if "trip_id" in request.GET:
+            stoptimes=[]
+            trip_id=request.GET['trip_id']
+            schedules=TripUpdates.objects.filter(trip_id=trip_id)
+            for schedule in schedules:
+                stoptime={}
+                print schedule.stop_id
+                stoptime["stop_name"]=schedule.stop_id.stop_name
+                stoptime["arrival_time"]=schedule.arrival_time
+                stoptime["departure_time"]=schedule.departure_time
+                stoptimes.append(stoptime)
+        return render_to_response('predictions.html',{'stoptimes':stoptimes})
+    except:
+        e = sys.exc_info()[0]
+        return render_to_response('results.html',{'error':e})
 def findserviceId(date):
     date=date.date()
     result=[]
